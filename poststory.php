de18bc557b8e94a12a3a099517a133794b2ad048
ameerothman <arothman@wustl.edu>
2021-02-19 11:55:12 -0600

    story upload

---
+++ poststory.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Module 3 Group</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1> Upload Your Story Here! </h1>
+    <form action="main.php" method="Post" id="uploadstory">
+        <label for="title">Title:</label>
+        <input type="text" name="title" id ="title">
+
+        <label for="storytext">Story Text:</label>
+        <textarea id="storytext" name="storytext" rows="30" cols="50"></textarea>
+        <button type="submit" id="logout-button">Upload Here</button>
+    </form>
+
+
+</body>
+</html>

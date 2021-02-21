bf3b1d1df3ba7d830c55ebe324cb0d5f54ab1bde
ameerothman <arothman@wustl.edu>
2021-02-20 19:29:40 -0600

    day 3

--- main.php
+++ main.php
@@ -15,13 +15,50 @@
         <button type="submit" id="logout-button">Post Story</button>
     </form>
 
+
    
 
 <?php
     require 'database.php';
     session_start();
     $user_id = $_SESSION['user_id'];
+
+    echo"<h2>$user_id</h2>";
+
+    $stmt = $mysqli->prepare("select * from stories");
+    if(!$stmt){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
+    }
     
+    $stmt->execute();
+    
+    $stmt->bind_result($username, $title, $body, $link);
+    
+    
+    while($stmt->fetch()){
+    echo "<div class='story'>\n";
+       echo $username;
+       echo "<h3 class='storytitle'>$title</h3>";
+       echo "<p class='storybody'>$body</p>";
+       echo "<a href='$link' class='storylink'>$link</a>";
+       echo "</div>\n";
+    }
+    
+    
+    $stmt->close();
+
+    // $stmt = $mysqli->prepare("SELECT COUNT(*), username, title, body, link FROM stories");
+
+    //     // Bind the results
+    // $stmt->bind_result($username, $title, $body, $link);
+        
+    // echo $stmt;
+
+    // $stmt->fetch();
+    // $stmt->close();
+
+
 ?>
 
 
--- poststory.php
+++ poststory.php
@@ -6,14 +6,49 @@
 </head>
 <body>
     <h1> Upload Your Story Here! </h1>
-    <form action="main.php" method="Post" id="uploadstory">
+    <form action="poststory.php" method="Post" id="uploadstory">
+
         <label for="title">Title:</label>
         <input type="text" name="title" id ="title">
 
-        <label for="storytext">Story Text:</label>
-        <textarea id="storytext" name="storytext" rows="30" cols="50"></textarea>
+        <label for="body">Story Text:</label>
+        <textarea id="body" name="body" rows="30" cols="50"></textarea>
+
+        <label for="link">Link:</label>
+        <input type="url" name="link" id ="link">
+
         <button type="submit" id="logout-button">Upload Here</button>
     </form>
+
+    <?php
+         require 'database.php';
+         session_start();
+        
+         $username = $_SESSION['user_id'];
+         $title = $_POST['title'];
+         $body = $_POST['body'];
+         $link = $_POST['link'];
+
+ 
+         $stmt = $mysqli->prepare("insert into stories (username, title, body, link) values (?, ?, ?, ?)");
+         if(!$stmt){
+             printf("Query Prep Failed: %s\n", $mysqli->error);
+             exit;
+         }
+         else if (!empty($_POST)){
+             echo "<p>Post successfully created.</p>";
+             echo '
+             <form action="main.php" method="Post">
+                 <button type="submit">Return to Main Page</button>
+             </form>';
+         }
+ 
+         $stmt->bind_param('ssss',  $username, $title, $body, $link);
+ 
+         $stmt->execute();
+         
+         $stmt->close();
+    ?>
 
 
 </body>

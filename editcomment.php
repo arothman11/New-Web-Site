ed81ff4703998d0515433079174815a274938222
Emily McClanahan <emilymcclanahan@wustl.edu>
2021-02-21 20:14:36 -0600

    adding comments

--- editcomment.php
+++ editcomment.php
@@ -13,22 +13,26 @@
 <?php
     require 'database.php';
     session_start();
-
+    
+    //check token
     if(!hash_equals($_SESSION['token'], $_POST['token'])){
         die("Request forgery detected");
     }
 
+    //get data from form
     $story_username = $_POST["story_username"];
     $story_title = $_POST["story_title"];
     $comment = $_POST["comment"];
     $token = $_SESSION['token'];
     $your_username = $_SESSION['user_id'];
 
+    //htmlentities
     $story_username = htmlentities($story_username);
     $story_title = htmlentities($story_title);
     $comment = htmlentities($comment);
     $your_username = htmlentities($your_username);
 
+    //checking username
     if( !preg_match('/^[\w_\-]+$/', $your_username) ){
         echo "Invalid username";
         exit;
@@ -37,6 +41,7 @@
 
     $sestok =$_SESSION['token'];
 
+    //form for posting a comment
     echo 
        "<form action='postcomment.php' method='Post' id='postcomment'>
            <input type='hidden' name='story_username' value='$story_username' />
@@ -48,7 +53,7 @@
 
    
 
-
+    //query for deleting comments 
     $stmt = $mysqli->prepare("delete from comments where your_username='$your_username' && story_username='$story_username' && story_title='$story_title' && comment='$comment'");
         if(!$stmt){
             printf("Query Prep Failed: %s\n", $mysqli->error);

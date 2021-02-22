1fd5855157be24b276d9fc245397d0f2da03f8ae
Emily McClanahan <emilymcclanahan@wustl.edu>
2021-02-21 20:08:28 -0600

    adding comments and slight code changes

--- deletepost.php
+++ deletepost.php
@@ -6,17 +6,26 @@
         die("Request forgery detected");
     }
 
+    //getting data from form
     $title = $_POST["titledata"];
     $body = $_POST["bodydata"];
     $link = $_POST["linkdata"];
     $token = $_SESSION['token'];
     $username = $_SESSION['user_id'];
 
+    //html entities
+    $title = htmlentities("titledata");
+    $body = htmlentities("bodydata");
+    $link = htmlentities("linkdata");
+    $username = htmlentities('user_id');
+
+    //check username
     if( !preg_match('/^[\w_\-]+$/', $username) ){
         echo "Invalid username";
         exit;
     }
-
+    
+    //query for deleting comments
     $stmt1 = $mysqli->prepare("delete from comments where story_username='$username' && story_title='$title'");
         if(!$stmt1){
             printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -27,6 +36,7 @@
          
         $stmt1->close();
 
+    //query for deleting stories
     $stmt = $mysqli->prepare("delete from stories where username='$username' && title='$title'");
         if(!$stmt){
             printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -36,6 +46,7 @@
         $stmt->execute();
          
         $stmt->close();
-
+        
+        //go back to main
         header("Location: main.php");
 ?>
